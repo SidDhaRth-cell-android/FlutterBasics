c5458fd3c9b6f3f673f37af5ac3bb9d1eec08909
Siddharth Singh <siddharth.singh@kobil.com>
2024-11-24 10:46:03 +0530

    - Text  - TextFormField  - Buttons  - Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,53 @@ class HelloWorld extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World"),
+        ),
         body: Center(
-          child: Text("Hello World"),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Hello World"),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      print("Button Called");
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        )),
+                    child: Text(
+                      "Log in",
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter Email",
+                  hintText: "Please enter your mail"
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter Password"
+                ),
+              ),
+              Image.network("https://picsum.photos/200/300"),
+              Image.asset("assets/success.png")
+            ],
+          ),
         ),
       ),
     );
