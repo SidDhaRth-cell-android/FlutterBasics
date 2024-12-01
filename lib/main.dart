ec3e56e256481cc429ced0cb7e819e49ed4ee81d
Siddharth Singh <siddharth.singh@kobil.com>
2024-12-01 12:08:28 +0530

    - Adds google authenticator

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter09/phone_main_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
-  runApp(HelloWorld());
+  runApp(MyApp());
 }
 
-class HelloWorld extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PhoneMainScreen(),
+      home: TextWidget(),
     );
   }
 }
 
-class Testing extends StatelessWidget {
+class TextWidget extends StatefulWidget {
+  @override
+  State<TextWidget> createState() => _TextWidgetState();
+}
+
+class _TextWidgetState extends State<TextWidget> {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello World"),
-      ),
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("Hello World"),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {
-                    print("Button Called");
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  child: Text(
-                    "Log in",
-                    style: TextStyle(color: Colors.white),
-                  )),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Enter Email",
-                  hintText: "Please enter your mail"),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Enter Password"),
-            ),
-            Image.network("https://picsum.photos/200/300"),
-            Image.asset("assets/success.png")
-          ],
-        ),
-      ),
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            onPressed: () {
+              _handleSignIn();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Google',
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.search,
+                  color: Colors.white,
+                )
+              ],
+            )),
+      )),
     );
   }
 }
